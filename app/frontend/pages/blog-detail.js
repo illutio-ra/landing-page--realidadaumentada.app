9a70343aa563762de8f5b7d7e19f405ec227a0bd
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2019-06-05 19:18:32 -0500

    Fix

---
+++ app/frontend/pages/blog-detail.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import PageComponent from '~base/page-component'
+import { FormattedMessage, injectIntl } from 'react-intl'
+import api from '~base/api'
+import { forcePublic } from '~base/middlewares/'
+import Link from '~base/router/link'
+import {
+  MegadraftEditor,
+  editorStateFromRaw,
+  editorStateToJSON,
+} from 'megadraft'
+
+class Blog extends PageComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ...this.baseState,
+      name: 'Eric',
+      unreadCount: 1000,
+      post: [],
+      content: null,
+    }
+  }
+
+  async componentDidMount() {
+    await this.loadData()
+  }
+
+  async loadData() {
+    const { data } = await api.get(
+      `/public/blog/${this.props.match.params.slug}`,
+    )
+
+    this.setState({
+      post: data,
+      content: editorStateFromRaw(JSON.parse(data.content)),
+    })
+  }
+
+  render() {
+    const basicStates = super.getBasicStates()
+    if (basicStates) {
+      return basicStates
+    }
+
+    const { name, unreadCount, post, content } = this.state
+    const { formatMessage } = this.props.intl
+
+    return (
+      <div className="container">
+        <div className="has-background-white">
+          <div className="section section-blog">
+            {content && (
+              <MegadraftEditor
+                plugins={[]}
+                readOnly={true}
+                editorState={content}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Blog.config({
+  path: '/blog/:slug',
+  title: 'Blog',
+  validate: forcePublic,
+  exact: true,
+})
+
+export default injectIntl(Blog)
