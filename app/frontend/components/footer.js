875e049e5a823b4525cf50f4653d80e028cdfe0a
Emmanuel Bernal <emmanuel_bernalv@hotmail.com>
2019-06-20 23:28:20 -0500

    fix footer

--- app/frontend/components/footer.js
+++ app/frontend/components/footer.js
@@ -76,8 +76,9 @@ class Footer extends Component {
               </div>
             </div>
           </div>
-          <div className="column is-6 vertically-centered is-flex-1">
-            <div className="content mail-chimp">
+          <div className="column is-6 vertically-centered">
+            {/* Disabled because we do not have access to mailchimp */}
+            {/* <div className="content mail-chimp">
               <div className="field is-grouped">
                 <p className="control is-expanded">
                   <input
@@ -92,27 +93,31 @@ class Footer extends Component {
                   </a>
                 </p>
               </div>
-            </div>
+            </div> */}
             <div className="vertically-bottom">
-              <Image src="/public/img/ar-footer.svg" alt="arcam" />
+              <Image
+                src="/public/img/ar-footer.svg"
+                alt="arcam"
+                className="img-centered"
+              />
             </div>
           </div>
-          <div className="column is-flex">
+          <div className="column section">
             <div className="content has-text-centered directory">
               <div className="columns">
-                <div className="column is-half is-offset-10">
+                <div className="column is-half">
                   <p>
                     <a href="/" className="is-link">
                       <FormattedMessage id="general.link_home" />
                     </a>
                   </p>
                   <p>
-                    <a href="#solutions" className="is-link">
+                    <a href="/#solutions" className="is-link">
                       <FormattedMessage id="general.link_solutions" />
                     </a>
                   </p>
                   <p>
-                    <a href="#blog" className="is-link">
+                    <a href="/#blog" className="is-link">
                       <FormattedMessage id="general.link_blog" />
                     </a>
                   </p>
@@ -129,12 +134,12 @@ class Footer extends Component {
                 </div>
                 <div className="column is-half">
                   <p>
-                    <a href="#app" className="is-link">
+                    <a href="/#porfolio" className="is-link">
                       <FormattedMessage id="general.link_app" />
                     </a>
                   </p>
                   <p>
-                    <a href="#pricing" className="is-link">
+                    <a href="/#pricing" className="is-link">
                       <FormattedMessage id="general.link_pricing" />
                     </a>
                   </p>
@@ -151,48 +156,46 @@ class Footer extends Component {
                 </div>
               </div>
             </div>
-            <div className="vertically-bottom">
-              <div className="columns is-mobile">
-                <div className="column">
-                  <a
-                    href="https://facebook.com/illutio"
-                    className="media-links"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fa fa-facebook" />
-                  </a>
-                </div>
-                <div className="column">
-                  <a
-                    href="https://twitter.com/illut_io"
-                    className="media-links"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fa fa-twitter" />
-                  </a>
-                </div>
-                <div className="column">
-                  <a
-                    href="https://www.instagram.com/illutio/"
-                    className="media-links"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fa fa-instagram" />
-                  </a>
-                </div>
-                <div className="column">
-                  <a
-                    href="https://www.youtube.com/user/ILLUTIO1"
-                    className="media-links"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fa fa-youtube-play" />
-                  </a>
-                </div>
+            <div className="columns is-mobile">
+              <div className="column is-offset-1">
+                <a
+                  href="https://facebook.com/illutio"
+                  className="media-links"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-facebook" />
+                </a>
+              </div>
+              <div className="column">
+                <a
+                  href="https://twitter.com/illut_io"
+                  className="media-links"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-twitter" />
+                </a>
+              </div>
+              <div className="column">
+                <a
+                  href="https://www.instagram.com/illutio/"
+                  className="media-links"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-instagram" />
+                </a>
+              </div>
+              <div className="column">
+                <a
+                  href="https://www.youtube.com/user/ILLUTIO1"
+                  className="media-links"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-youtube-play" />
+                </a>
               </div>
             </div>
           </div>
